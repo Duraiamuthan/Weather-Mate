72ef099ac67dacece06d5601f64fc5904fbd7b5c
Duraiamuthan Harikrishnan <h.duraiamuthan@gmail.com>
2020-12-22 14:58:34 -0800

    Added unit-testcases

---
+++ WeatherMate/WeatherMateTests/WeatherMateFakeTests.swift
@@ -0,0 +1,64 @@
+//
+//  WeatherMateFakeTests.swift
+//  WeatherMateTests
+//
+//  Created by Duraiamuthan Harikrishnan on 22/12/20.
+//
+
+import XCTest
+@testable import WeatherMate
+
+class WeatherMateFakeTests: XCTestCase {
+    var sut: SearchCityViewController!
+    
+    func testDecoding() throws {
+        /// When the Data initializer is throwing an error, the test will fail.
+        let testBundle = Bundle(for: type(of: self))
+        let path = testBundle.path(forResource: "Citylist", ofType: "json")
+        let jsonData = try? Data(contentsOf: URL(fileURLWithPath: path!), options: .alwaysMapped)
+     
+
+        /// The `XCTAssertNoThrow` can be used to get extra context about the throw
+        XCTAssertNoThrow(try JSONDecoder().decode([CountryList].self, from: jsonData!))
+    }
+    
+    
+    func testAllCityLoaded() {
+        let playData = CountryList.getAllCity()
+        XCTAssertEqual(playData.count, 0, "Data is not loaded")
+    }
+    
+    override func tearDown() {
+      sut = nil
+      super.tearDown()
+    }
+    
+    func test_UpdateSearchResults_ParsesData() {
+      // given
+      let promise = expectation(description: "Status code: 200")
+      
+      // when
+      XCTAssertEqual(sut.cityList.count, 0, "searchResults should be empty before the data executed")
+         
+     /* let url = URL(string: " ")
+      let dataTask = sut.defaultSession.dataTask(with: url!) {
+        data, response, error in
+        // if HTTP request is successful, call updateSearchResults(_:) which parses the response data into Tracks
+        if let error = error {
+          print(error.localizedDescription)
+        } else if let httpResponse = response as? HTTPURLResponse,
+          httpResponse.statusCode == 200 {
+          self.sut.updateSearchResults(data)
+        }
+        promise.fulfill()
+      }
+      dataTask.resume() */
+      wait(for: [promise], timeout: 5)
+      
+      // then
+      XCTAssertEqual(sut.cityList.count, 3, "Didn't parse 3 items from fake response")
+    }
+     
+
+}
+
--- WeatherMate/WeatherMateTests/WeatherMateTests.swift
+++ WeatherMate/WeatherMateTests/WeatherMateTests.swift
@@ -10,24 +10,44 @@ import XCTest
 
 class WeatherMateTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var sut: URLSession!
+    
+    override func setUp() {
+      super.setUp()
+      sut = URLSession(configuration: .default)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+      sut = nil
+      super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    // Asynchronous test: success fast, failure slow
+    func testValidCallToOpenWeatheGetsHTTPStatusCode200() {
+      // given
+        guard let getURL = URL(string:  String(format:"%@10d@2x.png",pathURL.imagePath)) else { return }
+       // 1
+      let promise = expectation(description: "Status code: 200")
+      
+      // when
+      let dataTask = sut.dataTask(with: getURL) { data, response, error in
+        // then
+        if let error = error {
+          XCTFail("Error: \(error.localizedDescription)")
+          return
+        } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+            if case 200...300 = statusCode  {
+            // 2
+            promise.fulfill()
+          } else {
+            XCTFail("Status code: \(statusCode)")
+          }
         }
+      }
+      dataTask.resume()
+      // 3
+      wait(for: [promise], timeout: 5)
     }
+    
 
 }
